9d7ab01ef878aec1e3b5428a27478f6fc3b49fb9
yangjunsik <wnstlr1379@naver.com>
2025-02-27 15:01:35 +0900

    띄어쓰기 수정

--- src/main/java/com/food/controller/PointController.java
+++ src/main/java/com/food/controller/PointController.java
@@ -4,8 +4,6 @@ import com.food.config.jwt.token.JwtUtil;
 import com.food.service.PaymentService;
 import com.food.service.PointService;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.enums.ParameterIn;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
